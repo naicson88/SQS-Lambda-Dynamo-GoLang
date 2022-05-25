b29ea9526d986839d6ef3cdf9af886b1c0a446e1
a91628319 <naicson88@hotmail.com>
2022-05-25 19:56:23 -0300

    first

---
+++ dynamoUpload/dynamoUpload.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"context"
+	"os"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+)
+
+var dynamo *dynamodb.DynamoDB
+var table, region string
+
+func init() {
+	table = os.Getenv("TABLE")
+	region = os.Getenv("REGION")
+
+	dynamo = dynamodb.New(session.Must(session.NewSession(&aws.Config{
+		Region: aws.String(region),
+	})))
+}
+
+func main() {
+	lambda.Start(Handler)
+}
+
+func Handler(ctx context.Context, sqsEvent events.SQSEvent) (err error) {
+	for _, message := range sqsEvent.Records {
+		err = putEvent(message)
+		if err != nil {
+			return err
+		}
+	}
+
+	return err
+}
+
+func putEvent(message events.SQSMessage) error {
+	_, err := dynamo.PutItem(&dynamodb.PutItemInput{
+		Item: map[string]*dynamodb.AttributeValue{
+			"Link": {
+				S: message.MessageAttributes["Link"].StringValue,
+			},
+			"Key": {
+				S: message.MessageAttributes["Key"].StringValue,
+			},
+			"S3Link": {
+				S: message.MessageAttributes["S3Link"].StringValue,
+			},
+		},
+
+		TableName: aws.String(table),
+	})
+
+	return err
+}
---
+++ s3Upload/s3upload.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/sqs"
+)
+
+var s3session *s3.S3
+var sqsQueue *sqs.SQS
+var region, bucket, queueURL string
+
+type InputEvent struct {
+	Link string `json:"link"`
+	Key  string `json:"key"`
+}
+
+type OutputEvent struct {
+	Link   string `json:"link"`
+	Key    string `json:"key"`
+	S3Link string `json:"s3link"`
+}
+
+func init() {
+	region = os.Getenv("REGION")
+	bucket = os.Getenv("BUCKET")
+	queueURL = os.Getenv("QUEUE")
+
+	sess := session.Must(session.NewSession(&aws.Config{
+		Region: aws.String(region),
+	}))
+
+	s3session = s3.New(sess)
+	sqsQueue = sqs.New(sess)
+}
+
+func main() {
+	lambda.Start(Handler)
+}
+
+func Handler(ctx context.Context, event InputEvent) (err error) {
+	image, err := getImage(event.Link)
+	if err != nil {
+		return err
+	}
+
+	_, err = s3session.PutObject(&s3.PutObjectInput{
+		Body:   bytes.NewReader(image),
+		Bucket: aws.String(bucket),
+		Key:    aws.String(event.Key),
+	})
+
+	if err != nil {
+		return err
+	}
+
+	var output = OutputEvent{
+		Key:  event.Key,
+		Link: event.Link,
+	}
+
+	output.S3Link = fmt.Sprintf("https://%s.%s.amazonaws.com/%s", bucket, region, output.Key)
+
+	_, err = sqsQueue.SendMessage(&sqs.SendMessageInput{
+		MessageAttributes: map[string]*sqs.MessageAttributeValue{
+			"Link": &sqs.MessageAttributeValue{
+				DataType:    aws.String("String"),
+				StringValue: aws.String(output.Link),
+			},
+			"Key": &sqs.MessageAttributeValue{
+				DataType:    aws.String("String"),
+				StringValue: aws.String(output.Key),
+			},
+			"S3Link": &sqs.MessageAttributeValue{
+				DataType:    aws.String("String"),
+				StringValue: aws.String(output.S3Link),
+			},
+		},
+
+		MessageBody: aws.String("New Event!"),
+		QueueUrl:    aws.String(queueURL),
+	})
+
+	return err
+}
+
+func getImage(url string) (bytes []byte, err error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return bytes, err
+	}
+
+	defer resp.Body.Close()
+
+	bytes, err = ioutil.ReadAll(resp.Body)
+	return bytes, err
+}
